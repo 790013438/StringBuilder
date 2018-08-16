c42a1c7d5bbbc63641545d247186a0ddbd503bd6
790013438 <790013438@qq.com>
2018-08-16 10:23:15 +0800

    junit 5. 测试StringBuilder不安全

--- string-builder/src/test/java/io/github/floyd/AppTest.java
+++ string-builder/src/test/java/io/github/floyd/AppTest.java
@@ -1,20 +1,40 @@
 package io.github.floyd;
 
-import static org.junit.Assert.assertTrue;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.RepeatedTest;
 
-import org.junit.Test;
+import java.util.function.BiFunction;
+import java.util.function.Supplier;
+import java.util.stream.Stream;
 
 /**
- * Unit test for simple App.
+ * 验证StringBuilder不是线程安全的
  */
-public class AppTest 
-{
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
+public class AppTest {
+
+    @RepeatedTest(10000)
+    public void testStringBuilder() {
+        testAbstractStringBuilder(new StringBuilder(), StringBuilder::append);
+    }
+
+    @RepeatedTest(10000)
+    public void testStringBuffer() {
+        testAbstractStringBuilder(new StringBuffer(), StringBuffer::append);
+    }
+
+    private <T extends CharSequence> void testAbstractStringBuilder(T builder, BiFunction<T, ? super String, T> accumulator) {
+        final long SIZE = 1000;
+        final Supplier<String> GENERATOR = () -> "a";
+
+        final CharSequence sequence = Stream
+            .generate(GENERATOR)
+            .parallel()
+            .limit(SIZE)
+            .reduce(builder, accumulator, (b1, b2) -> b1);
+
+        Assertions.assertEquals(
+                SIZE * GENERATOR.get().length(),  // expected
+                sequence.toString().length()      // actual
+                );
     }
 }
